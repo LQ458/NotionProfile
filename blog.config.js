681473c10505a166cf2bef30c21f6811db533b5a
Leo Yihao Qin <yihao.qin17311-biph@basischina.com>
2025-04-12 17:00:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -312,18 +312,18 @@ const BLOG = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      name: 'Passing Your World',
+      artist: 'DelDen',
+      url: 'http://music.163.com/song/media/outer/url?id=2085507521.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://p2.music.126.net/R0_RVAHKe1ezVjJ2wUF2rA==/109951168942767444.jpg'
     },
     {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      name: '萤火之森',
+      artist: 'CMJ',
+      url: 'http://music.163.com/song/media/outer/url?id=1415706303.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://p2.music.126.net/jNh5VIYGzc74RFPMv_TouQ==/109951164615554163.jpg'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
